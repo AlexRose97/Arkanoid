d5682775dd4d06e256715c517c14027f5cf3cf0c
AlexRose97 <aexlopez@gmail.com>
2025-03-31 23:08:46 -0600

    feat(): clase 01.02. Movimiento de la bola_default

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /*
+             * 1. se desvincula de la barra del jugador
+             * 2. se agrega movimiento dinamico y se elimina la gravedad
+             * 3. se agrega fuerza de impulso
+             */
+            transform.SetParent(null);
+            rb.isKinematic = false;
+            rb.gravityScale = 0;
+            rb.AddForce(new Vector2(1, 1).normalized * 10, ForceMode2D.Impulse);
+        }
+    }
+}
