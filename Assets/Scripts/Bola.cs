07920f6283906b4a497e0c3f5c46a0d6e7d671fc
AlexRose97 <aexlopez@gmail.com>
2025-04-02 21:49:09 -0600

    feat(): clase 01.05. Reseteo de la bola_default

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Bola : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private GameObject pala;
+
+    private bool lanzar = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +18,7 @@ public class Bola : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && lanzar)
         {
             /*
              * 1. se desvincula de la barra del jugador
@@ -26,7 +29,13 @@ public class Bola : MonoBehaviour
             rb.isKinematic = false;
             rb.gravityScale = 0;
             rb.AddForce(new Vector2(1, 1).normalized * 10, ForceMode2D.Impulse);
+            lanzar = false;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ResetearBola();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -34,6 +43,24 @@ public class Bola : MonoBehaviour
         if (other.gameObject.CompareTag("bloque"))
         {
             Destroy(other.gameObject);
+        }
+    }
+
+    private void ResetearBola()
+    {
+        if (other.gameObject.CompareTag("ZonaMuerte"))
+        {
+            /*
+             * 0. se frena la velocidad
+             * 1. se cambia el tipo de movimiento
+             * 2. Se vincula con la pala(jugador)
+             * 3. se coloca en el origen la bola.
+             */
+            rb.linearVelocity = Vector2.zero;
+            rb.isKinematic = true;
+            transform.SetParent(pala.transform);
+            transform.localPosition = new Vector3(0, 1, 0);
+            lanzar = true;
         }
     }
 }
